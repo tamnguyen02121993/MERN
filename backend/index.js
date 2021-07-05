5c31de1136c4ae50f1235e9e02a2d0c99ab76ca8
tamnguyen <tamnguyen02121993@gmail.com>
2021-07-05 18:50:08 +0700

    Add backend side

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
+//Config env load environment variables
+dotenv.config();
+
+const MongoClient = mongodb.MongoClient;
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+    poolSize: 50,
+    wtimeout: 2500,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).catch(error => {
+    console.error(error.stack);
+    process.exit(1);
+}).then(async (client) => {
+    await RestaurantsDAO.injectDB(client);
+    app.listen(port, () => `listening on the port: ${port}`)
+})
